6dd42120f47db9295150ff9a3239bdbde35e7418
Wesley Wei <wesleytianqiwei@gmail.com>
2022-08-09 17:37:31 -0400

    classnames library to compose lists of classe

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,16 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames('button', {   
+   'button--confirm' : props.confirm,
+   'button--danger' : props.danger
+   });
+
+   return <button  
+      onClick={props.onClick} 
+      className={buttonClass} 
+      disabled={props.disabled}>{props.children} </button>;
 }
